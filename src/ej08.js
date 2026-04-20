6d7f0e280583309b4cadf97407282c0059271ef5
MARTIN URIEL GRINFELD <49550833@ort>
2026-04-20 12:09:07 -0300

    intento de ej08

---
+++ src/ej08.js
@@ -0,0 +1,10 @@
+import {OMDBSearchByPage, eb26d84e} from './modules/omdb-wrapper.js'
+
+
+let resultado = null;
+
+// Test de la función OMDBSearchByPage..
+
+resultado  = await OMDBSearchByPage("cars", 1);
+
+console.log("OMDBSearchByPage", resultado);
